81d80252def94dbb797d7e8942f4acb457f15a89
herdicDiaz <herdic_28@hotmail.com>
2019-03-03 18:02:59 -0500

    reportes con su generador

--- Uniamazonia_aprende/Uniamazonia Juego/Models/Modulo.cs
+++ Uniamazonia_aprende/Uniamazonia Juego/Models/Modulo.cs
@@ -104,7 +104,9 @@ namespace Uniamazonia_Juego.Models
 
         public Boolean eliminar_modulo()
         {
-            String Query = "update modulo set estado_modulo='D' where nombre_modulo='" + nombre_modulo + "';";
+            String query = "select id_modulo from modulo where nombre_modulo='"+nombre_modulo+"';";
+            int id_modulo_aux = conexion.buscar_ID_BD(query);
+            String Query = "call eliminar_en_cascada('"+id_modulo_aux+"')";
             if (conexion.delete_BD(Query))
 
             {
--- Uniamazonia_aprende/Uniamazonia Juego/Web_forms_reports/ReporteListaSancion.aspx.cs
+++ Uniamazonia_aprende/Uniamazonia Juego/Web_forms_reports/ReporteListaSancion.aspx.cs
@@ -15,21 +15,27 @@ using System.Drawing;
 using CrystalDecisions.Web;
 using CrystalDecisions.Shared;
 using System.Data;
-
+using Uniamazonia_Juego.Controllers;
 namespace Uniamazonia_Juego.Web_forms_reports
 {
     public partial class ReporteListaSancion : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Connection con = new Connection();
+            Connection con = new Connection();http://localhost:58475/Web_forms_reports/ReporteListaSancion.aspx.cs
             DataTable dt = new DataTable();
             DataSet ds = new DataSet();
             CrystalReportSancion cr = new CrystalReportSancion();
+            AdministradorController administradorC = new AdministradorController();
 
             dt = con.consultar_BD("Select *from sancion where estado_sancion=0;");
             ds.Tables.Add(dt);
             cr.SetDataSource(ds.Tables[0]);
+            int id_administrador =Convert.ToInt32( Session["id_usuario"].ToString());
+            DataTable consulta = administradorC.ConsultaParametroFkUsuario(id_administrador);
+            String nombre1 = consulta.Rows[0]["nombres_admin"].ToString();
+            String apellidos = consulta.Rows[0]["apellidos_admin"].ToString();
+            cr.SetParameterValue("autor", nombre1 + apellidos );
             CrystalReportViewerSancion.ReportSource = cr;
 
         }
